05e6f8f9bc025ac7a935258b83eb6efa75038e0a
Olivier Chafik <ochafik@users.noreply.github.com>
2017-11-29 14:14:26 +0000

    Update docker-build.sh

--- rpi/bazel/docker-build.sh
+++ rpi/bazel/docker-build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# TODO: https://docs.docker.com/get-started/part2/
+# TODO: https://docs.docker.com/engine/userguide/eng-image/multistage-build/#before-multi-stage-builds
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build -t rpi-raspbian-bazel .
-docker cp `docker ps -l -q`:/bazel-src/bazel-bin/blaze blaze-armhf
+# docker cp `docker ps -l -q`:/bazel-src/bazel-bin/blaze blaze-armhf
 
 #oracle-java8-jdk openjdk-8-jdk 
 # sed -i 's/jessie/stretch/g' /etc/apt/sources.list && \
